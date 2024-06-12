9a5d23cc2ab09b77192df47005962f6371adb398
thokjend <thokjend@online.no>
2024-06-12 14:37:28 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace Workshops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var workShopsJson = File.ReadAllText("workshops.json");
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var workshops = JsonSerializer.Deserialize<WorkshopInfo[]>(workShopsJson, options);
+
+            Console.WriteLine(workshops[0].Bedriftsnavn);
+
+        }
+    }
+}
---
+++ WorkshopInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Workshops
+{
+    internal class WorkshopInfo
+    {
+        public string Bedriftsnavn { get; set; }
+        public string Adresse { get; set; }
+        public int Postnummer { get; set; }
+        public string Poststed { get; set; }
+        public string Godkjenningstyper { get; set; }
+        public int Organisasjonsnummer { get; set; }
+        public int Godkjenningsnummer { get; set; }
+
+    }
+}
